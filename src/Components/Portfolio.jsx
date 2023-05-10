881eda5f625311ff085c9458ed7c8e4ac95d15c6
Santiago Velasquez Chang <131800294+SantiagoVelasquezChangMLSA@users.noreply.github.com>
2023-05-10 20:08:51 +0000

    Editting with codespace

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "/workspaces/MLSA-definitivo/src/images/axp-photography-SML3jQ6nuk4-unsplash.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "landscape";
 
 /**
  * Project list
@@ -31,28 +31,21 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "AIR CONDITIONER SENSOR",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+      "This project implemented an IoT-based home automation system with microcontrollers connected to the internet for data acquisition and actuator control in a Smart City or Smart Campus. It requires designing and implementing a digital device system based on 5 interconnected microcontrollers programmed to exchange information using various protocols.",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
-    title: "Web Development for Beginners",
+    title: "SURVEY GAMIFICATION",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+      " Computational simulator that mimics the behavior of a business, economic, social, political, or educational system. Analyze current or future scenarios to improve the system's processes/components. The simulator will have a graphical interface like a 2D/3D video game.",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
     title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+    description: "Created with html and css. Includes my experience and design abilities.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
 
